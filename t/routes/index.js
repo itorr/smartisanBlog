1a8b8cd69d10b1751d3c32377ce7c9a608321a9b
x-pengg <qq710843409>
2016-10-16 21:01:22 +0800

    润下色

--- t/routes/index.js
+++ t/routes/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
+const express = require('express'),
+    router = express.Router(),
+    superagent = require('superagent'),
+    eventproxy = require('eventproxy');
 
-const superagent = require('superagent');
-const async = require('async');
 
 const account = require('../config.json');
+
 
 const url = {
     getCookie: 'https://account.smartisan.com/v2/session/?m=post',
@@ -19,150 +19,77 @@ const header = {
 };
 
 
-router.get('/', (req, res) => {
-    async.waterfall([
-        (callback) => {
-            superagent
-                .post(url.getCookie)
-                .set(header)
-                .set('Referer', 'https://account.smartisan.com/')
-                .send(account)
-                .redirects(0)
-                .end((err, response) => {
-                    if (err || !response.ok) {
-                        console.log('出错了!');
-                    } else if (response.body.errno == 1502) {
-                        console.log('CAPTCHA REQUIRED');
-                        res.send(JSON.stringify(response.body));
-                    } else {
-                        console.log('成功获取了 uid ' + JSON.stringify(response.body));
-                        var token = response.headers['set-cookie']
-                            .join(',').match(/SCA_SESS=(\w{16,64})/i)[1];
-                        console.log('成功获取了 token '+ token);
-                        callback(null, token);
-                    }
-                })
-        },
-        (arg1, callback) => {
-            // arg1 now equals 'token'
-            // callback(null, 'profile');
-            superagent
-                .get(url.getProfile)
-                .set(header)
-                .set('Referer', 'https://cloud.smartisan.com/')
-                .set('Cookie', 'SCA_SESS=' + arg1 + '-a; SCA_LOGIN=1')
-                .redirects(0)
-                .end((err, response) => {
-                    if (err || !response.ok) {
-                        console.log('出错了!');
-                    } else if (response.body.code == 1) {
-                        console.log('未登录');
-                    } else {
-                        console.log('成功获取了资料:\n ' + JSON.stringify(response.body));
-                        var profile = JSON.stringify(response.body);
-                        callback(null, profile);
-                    }
-                })
+var ep = new eventproxy();
+
+var ticket =  {
+    value: null,
+    expires: null,
+    _outOfDate : function () {
+        return this.expires < new Date();
+    },
+    _init: function () {
+        var that = this;
+        superagent
+            .post(url.getCookie)
+            .set(header)
+            .set('Referer','https://account.smartisan.com/')
+            .send(account)
+            .redirects(0)
+            .end((err, res)=> {
+            if (err || !res.ok) {
+            console.log('出错了!');
+        } else {
+            console.log('成功获取了 uid' + JSON.stringify(res.body));
+
+            that.value = res.headers['set-cookie']
+                .join(',').match(/SCA_SESS=(\w{16,64})/i)[1];
+            that.expires = new Date().addMonth(7);
+
+            console.log(that.value);
+
+            ep.emit('got_ticket', that.value);
         }
-    ], (err, result) => {
-        // result now equals 'profile'
-        res.send(result);
     });
+    }
+};
+
+
+Date.prototype.addMonth = function (monthNum) {
+    var date = new Date(this);
+    date.setDate(this.getDate()+monthNum);
+    return date;
+};
+
+
+var execute = (method, res)=> {
+    if (ticket._outOfDate()) {
+        console.log('ticket 过期了!');
+        ticket._init();
+        ep.all('got_ticket', (_ticket)=> {
+            method(_ticket, res);
+    });
+    }else{
+        method(ticket.value, res)
+    }
+};
+
+var getList = (ticket, res)=> {
+    let _res = res;
+    superagent
+        .post(url.getNote)
+        .set(header)
+        .set('Referer','https://cloud.smartisan.com/apps/note/')
+        .set('SCA_SESS', ticket)
+        .set('SCA_LOGIN', 1)
+        .set('Cookie', 'SCA_SESS='+ticket+'-a; SCA_LOGIN=1')
+        .end((err, res)=>{
+        console.log('execute profiles: ' + JSON.stringify(res.body));
+    _res.send(JSON.stringify(res.body))
 });
+};
 
-
-router.get('/note', (req, res) => {
-    async.waterfall([
-        (callback) => {
-            superagent
-                .post(url.getCookie)
-                .set(header)
-                .set('Referer', 'https://account.smartisan.com/')
-                .send(account)
-                .redirects(0)
-                .end((err, response) => {
-                    if (err || !response.ok) {
-                        console.log('出错了!');
-                    } else if (response.body.errno == 1502) {
-                        console.log('太频繁了!');
-                        res.send(JSON.stringify(response.body));
-                    } else {
-                        console.log('成功获取了 uid ' + JSON.stringify(response.body));
-                        var token = response.headers['set-cookie']
-                            .join(',').match(/SCA_SESS=(\w{16,64})/i)[1];
-                        console.log('成功获取了 token '+ token);
-                        callback(null, token);
-                    }
-                })
-        },
-        (arg1, callback) => {
-            // arg1 now equals 'token'
-            // callback(null, 'note');
-            superagent
-                .get(url.getNote)
-                .set(header)
-                .set('Referer', 'https://cloud.smartisan.com/')
-                .set('Cookie', 'SCA_SESS=' + arg1 + '-a; SCA_LOGIN=1')
-                .redirects(0)
-                .end((err, response) => {
-                    if (err || !response.ok) {
-                        console.log('出错了!');
-                    } else if (response.body.code == 1) {
-                        console.log('未登录');
-                    } else {
-                        console.log('成功获取了便签:\n ' + JSON.stringify(response.body));
-                        var note = response.body;
-                        callback(null, note);
-                    }
-                })
-        }
-    ], (err, result) => {
-        res.send(result);
-    });
+router.get('/list', (req, res)=> {
+    execute(getList, res);
 });
 
 module.exports = router;
-
-
-
-// var Token = function () {
-//
-//     return {
-//         value: null,
-//         expires: null,
-//         _outOfDate : function () {
-//             return this.expires < new Date();
-//         },
-//         _init: function () {
-//             var that = this;
-//             superagent
-//                 .post(url.getCookie)
-//                 .set(header)
-//                 .set('Referer','https://account.smartisan.com/')
-//                 .send(account)
-//                 .redirects(0)
-//                 .end(function (err, response) {
-//
-//                     if (err || !response.ok) {
-//                         console.log('出错了!');
-//                     } else {
-//                         console.log('成功获取了 uid' + response.body);
-//
-//                         var cookie;
-//                         cookie.value = response.headers['set-cookie']
-//                             .join(',').match(/SCA_SESS=(\w{16,64})/i)[1];
-//                         Date.prototype.addDate = function (dateNum) {
-//                             var date = new Date(this);
-//                             date.setDate(this.getDate() + dateNum);
-//                             console.log('Cookie 将于 '+ date.toLocaleString() + ' 过期');
-//                             return date;
-//                         };
-//
-//                         cookie.expires = new Date().addDate(7);
-//
-//                         console.log(cookie.value);
-//                     }
-//                 });
-//         }
-//     }
-// };
