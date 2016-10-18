b4df13fa18317eaf704f30d50849c4cfafc1d317
solarhell <songjiaxin2008@gmail.com>
2016-10-18 16:05:00 +0800

    🔨1.0

--- t/routes/index.js
+++ t/routes/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const path = require('path')
 const express = require('express')
 const router = express.Router()
 const superagent = require('superagent')
@@ -10,7 +12,9 @@ const account = require('../config.json')
 const url = {
   getCookie: 'https://account.smartisan.com/v2/session/?m=post',
   getProfile: 'https://cloud.smartisan.com/index.php?r=account%2Flogin',
-  getNote: 'https://cloud.smartisan.com/apps/note/index.php?r=v2%2FgetList'
+  getNote: 'https://cloud.smartisan.com/apps/note/index.php?r=v2%2FgetList',
+  getImage: 'https://cloud.smartisan.com/notesimage/',
+  itorr: 'http://x.mouto.org/wb/x.php?up'
 }
 
 const header = {
@@ -34,14 +38,14 @@ var ticket = {
             .set('Referer', 'https://account.smartisan.com/')
             .send(account)
             .redirects(0)
-            .end((err, res) => {
-              if (err || !res.ok) {
+            .end((err, response) => {
+              if (err || !response.ok) {
                 console.log('出错了!')
               } else {
-                console.log('成功获取了 uid: ' + JSON.stringify(res.body))
-                console.log(res.headers['set-cookie'])
+                console.log('成功获取了 uid: ' + JSON.stringify(response.body))
+                console.log(response.headers['set-cookie'])
 
-                that.value = res.headers['set-cookie']
+                that.value = response.headers['set-cookie']
                     .join(',').match(/SCA_SESS=([\w\-]{16,64})/i)[1]
                 moment.locale('zh-cn')
                 const time = moment()
@@ -85,6 +89,7 @@ const getProfile = (ticket, res) => {
               'nickname': response.body['data']['nickname'],
               'avatar_url': response.body['data']['avatar_url']
             }
+            fs.writeFileSync('./output/info.json', JSON.stringify(payload, null, 2) , 'utf-8')
             res.send(JSON.stringify(payload))
           }
         })
@@ -110,10 +115,53 @@ const getNote = (ticket, res) => {
               }
             }
             console.log('成功输出' + payload.length + '条便签:\r')
-            console.log('输出是' + payload)
+            console.log('输出是' + JSON.stringify(payload))
+            fs.writeFileSync('./output/post.json', JSON.stringify(payload, null, 2), 'utf-8')
             res.send(payload)
           }
         })
+}
+
+let downCount = 0
+const downStatus = (totalCount, current) => {
+  ++downCount
+  console.log(current + '下载完成!')
+  console.log('历史的进程: ' + downCount + '/' + totalCount)
+  return totalCount === downCount
+}
+
+const getImage = (ticket, res) => {
+  downCount = 0
+  const post = fs.readFileSync('./output/post.json', 'utf-8')
+  const 图片们 = post.match(/Notes_\w+\.(png|jpeg|jpg|gif)/ig)
+  console.log(图片们)
+  const 锤子数组 = []
+  for (let offset = 0; offset < 图片们.length; offset++) {
+    锤子数组.push(url.getImage + 图片们[offset])
+  }
+  console.log(锤子数组)
+  fs.writeFileSync('./output/t.json', JSON.stringify(锤子数组, null, 2), 'utf-8')
+  for (let offset = 0; offset < 锤子数组.length; offset ++) {
+    console.log('我是第' + offset + '个' + 锤子数组[offset])
+    console.log('我是第' + offset + '个' + 图片们[offset])
+
+    superagent
+        .get(锤子数组[offset])
+        .set(header)
+        .set('Referer', 'https://cloud.smartisan.com/apps/note/')
+        .set('Cookie', 'SCA_SESS=' + ticket + '-a; SCA_LOGIN=1')
+        .end((err, response) => {
+          if (err || !response.ok) {
+            console.log('出错了!')
+          } else {
+            fs.writeFileSync('./pics/'+图片们[offset], response.body)
+            if (downStatus(图片们.length, 图片们[offset])) {
+              res.send('所有图片下载完成!\r\n' + 图片们)
+            }
+            console.log(response.body)
+          }
+        })
+  }
 }
 
 router.get('/info', (req, res) => {
@@ -122,6 +170,46 @@ router.get('/info', (req, res) => {
 
 router.get('/post', (req, res) => {
   execute(getNote, res)
+})
+
+router.get('/t', (req, res) => {
+  execute(getImage, res)
+})
+
+router.get('/pics', (req, res) => {
+  const post = fs.readFileSync('./output/post.json', 'utf-8')
+  const 图片们 = post.match(/Notes_\w+\.(png|jpeg|jpg|gif)/ig)
+  const payload = []
+  console.log(图片们.length)
+  for (let offset = 0; offset < 图片们.length; offset++) {
+    let file = fs.readFileSync('./pics/' + 图片们[offset])
+    superagent
+        .post(url.itorr)
+        .set('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.59 Safari/537.36')
+        .set('Content-Type', 'multipart/form-data')
+        .attach('file', file, './pics'+ 图片们[offset])
+        .end((err, response) => {
+          if (err || !response.ok) {
+            console.log('出错了!')
+          } else {
+            console.log('当前下标' + offset)
+            console.log('正在上传' + 图片们[offset])
+            console.log('一共有' + 图片们.length)
+
+            const a = {}
+            const key = 图片们[offset]
+            const value = response.body.pid
+            a[key] = value
+            payload.push(a)
+
+            if (offset === 图片们.length - 1) {
+              console.log(payload)
+              fs.writeFileSync('./output/pics.json', JSON.stringify(payload, null, 2), 'utf-8')
+              res.send('跟偷偷肉交易完毕!\r\n' + JSON.stringify(payload))
+            }
+          }
+        })
+  }
 })
 
 module.exports = router
